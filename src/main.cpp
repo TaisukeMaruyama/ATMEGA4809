c5b639fe32331181b6e9e833cab238c62db32043
TaisukeMaruyama <taisuke_maruyama@jp.honda>
2025-01-06 00:09:43 +0900

    reset button enable

--- src/main.cpp
+++ src/main.cpp
@@ -13,23 +13,43 @@
 #define TFT_RST 9
 #define TFT_WIDTH 160
 #define TFT_HEIGHT 80
+const int ButtonPin = 12;
+
+#define AS5600_AS5601_DEV_ADDRESS 0x36
+#define AS5600_AS5601_REG_RAW_ANGLE 0x0C
+#define AS5600_ZMC0 0x00
+#define AS5600_ZPOS 0x01
+#define AS5600_MPOS 0x03
+#define AS5600_MANG 0x05
+uint16_t zeroPosition = 0x0000;
+uint16_t maxAngle = 0x0400;
+
 
 #define AS5600_AS5601_DEV_ADDRESS 0x36
 #define AS5600_AS5601_REG_RAW_ANGLE 0x0C
 
+bool isReferenceSet = false;
 float currentAngle = 0;
-float previousAngle = -1;
-
+float previousHeight = -1;
+float initialAngle = 0;
+float relativeAngle = 0;
+float height = 0.0;
 
 Adafruit_ST7735 tft = Adafruit_ST7735(TFT_CS, TFT_DC, TFT_RST);
 
 float readEncoderAngle();
+void setZeroPosition(uint16_t zeroPosition);
+void setMaxAngle(uint16_t maxAngle);
+
 
 
 
 void setup() {
     Wire.begin();
     Wire.setClock(400000);
+
+    pinMode(ButtonPin,INPUT_PULLUP);
+    
 
     
     tft.initR(INITR_GREENTAB);
@@ -52,14 +72,41 @@ void loop() {
 
     currentAngle = readEncoderAngle();
 
-    if(currentAngle != previousAngle){
-        char currentText[10],previousText[10];
-        dtostrf(currentAngle,4,1,currentText);
-        dtostrf(previousAngle,4,1,previousText);
+    if(digitalRead(ButtonPin) == LOW){
+        initialAngle = readEncoderAngle();
+        isReferenceSet = true;
+    }
 
-        bool isSingleDigit = (currentText[1] == '.');
+    if(isReferenceSet == true){
+        relativeAngle = currentAngle - initialAngle;
+
+          if(relativeAngle > 1){
+    height = -0.951 * ((currentAngle - 360.0) - initialAngle)+0.01;
+    }else{
+    height = -0.951 * relativeAngle + 0.01;
+    }
+
+    // height = height + 3.0;
+    if(height < 0){
+    height = 0.0;
+    }
+    if(height > 50.0){
+    height = 0.0;
+    }
+
+     height = height + 3.18;
+       
+
+    }
+
+    if(height != previousHeight){
+        char heightText[10],previousText[10];
+        dtostrf(height,4,1,heightText);
+        dtostrf(previousHeight,4,1,previousText);
+
+        bool isSingleDigit = (heightText[1] == '.');
       
-        if(currentText[0] != previousText[0]){
+        if(heightText[0] != previousText[0]){
             tft.fillRect(31,51,40,45,ST7735_BLACK);
             tft.setFont(&FreeSans18pt7b);
             if(isSingleDigit){
@@ -68,30 +115,30 @@ void loop() {
             tft.setCursor(46,85);
             }
             
-            tft.print(currentText[0]);
+            tft.print(heightText[0]);
             // tft.print(currentText[1]);
                          
                 
         } 
-        if (currentText[1] != previousText[1]){
+        if (heightText[1] != previousText[1]){
             tft.fillRect(67,51,40,45,ST7735_BLACK);
             tft.setFont(&FreeSans18pt7b);
             tft.setCursor(68,85);
-            tft.print(currentText[1]);            
+            tft.print(heightText[1]);            
         }
 
         tft.setFont(&FreeSans18pt7b);
             tft.setCursor(85,85);
-        tft.print(currentText[2]);   
+        tft.print(heightText[2]);   
     
-      if(currentText[3] != previousText[3]){
+      if(heightText[3] != previousText[3]){
             tft.fillRect(94,51,23,45,ST7735_BLACK);
             tft.setFont(&FreeSans18pt7b);
                 tft.setCursor(95,85);
-            tft.print(currentText[3]);            
+            tft.print(heightText[3]);            
 
       }
-        previousAngle = currentAngle;
+        previousHeight = height;
         
     }
     
