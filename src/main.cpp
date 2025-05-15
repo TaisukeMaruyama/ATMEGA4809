1a9e09d7dd38e6291b903f406b1ecd25dcf091f8
TaisukeMaruyama <taisuke_maruyama@icloud.com>
2025-05-16 00:02:30 +0900

    add comment

--- src/main.cpp
+++ src/main.cpp
@@ -10,8 +10,8 @@
 #include <Fonts/FreeMonoBoldOblique9pt7b.h>
 #include <EEPROM.h>
 
+// IO settings //
 #define GreenLed 16 //powerLed
-
 #define TFT_CS  7
 #define TFT_DC  10
 #define TFT_SDA 4
@@ -22,6 +22,7 @@
 #define TFT_POWER_PIN 13
 const int ButtonPin = 12;
 
+// AS5600 register //
 #define AS5600_AS5601_DEV_ADDRESS 0x36
 #define AS5600_AS5601_REG_RAW_ANGLE 0x0C
 #define AS5600_ZMC0 0x00
@@ -30,16 +31,12 @@ const int ButtonPin = 12;
 #define AS5600_MANG 0x05
 uint16_t zeroPosition = 0x0000;
 uint16_t maxAngle = 0x0400; //maxAngle 90deg
-
-
 #define AS5600_AS5601_DEV_ADDRESS 0x36
 #define AS5600_AS5601_REG_RAW_ANGLE 0x0C
-#define DEG_TO_RAD 0.0174532925 
 
+// height variable //
 bool isReferenceSet = false;
-bool sleepMode = 0;
 static float heightForSleep = 0.0f;
-bool GreenLedState = false;
 float currentAngle = 0;
 float previousHeight = -1;
 float initialAngle = 0;
@@ -50,39 +47,41 @@ const float maxHeight = 50.0f;
 float height = 0.0;
 float smoothHeight = 0.0;
 const float smoothingFactor = 0.7f; //RC Fillter
-unsigned long lastUpdateTime = 0;
-unsigned long previousMillis = 0;
-int LedOnTime = 20;
-int LedOffTime = 3000;
+
+// power LED variable //
+bool GreenLedState = false;
 int fadeValue = 0;
 int fadeAmount = 10;
 bool fadeDirectionUp = true;
 
-
-
+// sleep variable //
+bool sleepMode = 0;
 unsigned long lastInteractionTime = 0;
 const unsigned long inactivityThreshold = 60000;
 const float sleepValue = 1.0;
 
+// battery variable
 int batteryThreshold = 340;
 
+// prototype //
 Adafruit_ST7735 tft = Adafruit_ST7735(TFT_CS, TFT_DC, TFT_RST);
-
 float readEncoderAngle();
 void setZeroPosition(uint16_t zeroPosition);
 void setMaxAngle(uint16_t maxAngle);
 
 
-
-
 void setup() {
+
+    // EEPROM settings
     restoreZeroPositionFromEEPROM();
     EEPROM.get(2,initialAngle);
     isReferenceSet = true;
 
+    // AS5600 MaxAngle settings
     setMaxAngle(maxAngle);
 
 
+    // I2C settings
     Wire.begin();
     Wire.setClock(400000);
 
@@ -91,13 +90,13 @@ void setup() {
     digitalWrite(TFT_POWER_PIN,HIGH);
 
     
-    tft.initR(INITR_GREENTAB);
+    tft.initR(INITR_GREENTAB); //for greentab setting
     tft.invertDisplay(true);
     tft.fillScreen(ST7735_BLACK);
     tft.setRotation(1);
     tft.setTextSize(1);
   
-    
+    // startup message
     tft.setTextColor(0xf7be);
     tft.setFont(&FreeMonoBoldOblique9pt7b);
     tft.setCursor(40,70);
@@ -113,17 +112,17 @@ void setup() {
 
 void loop() {
 
+    // battery indicate green or red //
     int BattValue = getBatteryRaw();
-
     static bool prevBatteryGood = false;
     bool batteryGood = (BattValue > batteryThreshold);
 
     if(batteryGood != prevBatteryGood){
         if(batteryGood == true){
-        tft.drawRoundRect(30, 30, 100, 70, 8, 0x2d13);
+        tft.drawRoundRect(30, 30, 100, 70, 8, 0x2d13); //green indicate
         tft.fillRoundRect(30, 30, 100, 23, 8, 0x2d13);        
     }else{
-        tft.drawRoundRect(30, 30, 100, 70, 8, 0xe003);
+        tft.drawRoundRect(30, 30, 100, 70, 8, 0xe003); //red indicate
         tft.fillRoundRect(30, 30, 100, 23, 8, 0xe003);   
     }
 
@@ -133,26 +132,26 @@ void loop() {
     tft.println("RideHeight");
 
     prevBatteryGood = batteryGood;
-}
+    }
 
-
-    
+    //height calucurate   
     currentAngle = readEncoderAngle();
 
-    if(digitalRead(ButtonPin) == LOW){
+     //carib calucrate 
+     if(digitalRead(ButtonPin) == LOW){
         float angle = readEncoderAngle();
         initialAngle = angle;
-
         saveCurrentZeroPositionToEEPROM();
         EEPROM.put(2,initialAngle);
         isReferenceSet = true;       
-    }
+      }
 
+    // height calucrate
     if(isReferenceSet == true){
       relativeAngle = currentAngle - initialAngle;
       float rerativeAngleRad = radians(relativeAngle);
       height = proveLength * tan(rerativeAngleRad) + 5.0f;
-
+     // height crop   
       if(height < minHeight){
         height = minHeight;
       }
@@ -161,28 +160,17 @@ void loop() {
       }
 
       smoothHeight = height; 
-    
-    
-    /*
-    if(height < 1.9f){
-    height = 1.9;
-    }
-    if(height > 50.0){
-    height = 50.0;
-    } 
-    */
-
     }
     
 
-
+    // anti flicker    
     if(smoothHeight != previousHeight){
         char heightText[10],previousText[10];
         dtostrf(smoothHeight,4,1,heightText);
         dtostrf(previousHeight,4,1,previousText);
 
         bool isSingleDigit = (heightText[1] == '.');
-      
+        
         if(heightText[0] != previousText[0]){
             tft.fillRect(33,56,38,41,ST7735_BLACK);
             tft.setFont(&FreeSans18pt7b);
@@ -190,35 +178,30 @@ void loop() {
             tft.setCursor(60,87);
             }else{
             tft.setCursor(46,87);
-            }
-            
-            tft.print(heightText[0]);
-            // tft.print(currentText[1]);                       
-                         
-              
+            }          
+            tft.print(heightText[0]);                       
         } 
+        
         if (heightText[1] != previousText[1]){
             tft.fillRect(67,56,40,41,ST7735_BLACK);
             tft.setFont(&FreeSans18pt7b);
             tft.setCursor(68,87);
             tft.print(heightText[1]);            
         }
-
         tft.setFont(&FreeSans18pt7b);
             tft.setCursor(85,87);
         tft.print(heightText[2]);   
     
-      if(heightText[3] != previousText[3]){
+        if(heightText[3] != previousText[3]){
             tft.fillRect(94,56,23,41,ST7735_BLACK);
             tft.setFont(&FreeSans18pt7b);
                 tft.setCursor(95,87);
-            tft.print(heightText[3]);            
-
-      }
+            tft.print(heightText[3]);
+        }
         previousHeight = smoothHeight;
-                
     }
 
+    // sleep control
     if(abs(smoothHeight - heightForSleep) > sleepValue){
         lastInteractionTime = millis();
         heightForSleep = smoothHeight;
@@ -232,9 +215,7 @@ void loop() {
         sleepMode = 0;
     }
 
-
     unsigned long currentMillis = millis();
-
 
     if(sleepMode == 1){
                 GreenLedState = false;
